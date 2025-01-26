60676950a9210acbe61e2fe077edf7e2aaf574e8
Dominik Ospelt <dominik.ospelt@googlemail.com>
2025-01-26 13:26:02 +0100

    added main menu

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -15,14 +15,41 @@ public class GameController : MonoBehaviour
     [SerializeField]
     public Teamscore[] teamscores;
 
+    [SerializeField]
+    private ControlSchemeSetup _controlSetup;
+
+    [SerializeField]
+    private GgjController _playerOne;
+
+    [SerializeField]
+    private GgjController _playerTwo;
+
+    [SerializeField]
+    private GgjController _playerThree;
+
+    [SerializeField]
+    private GgjController _playerFour;
+
     private void Start () {
         SetTeamColors ();
+        SetPlayerControls ();
     }
 
     private void SetTeamColors () {
         for ( int i = 0; i < teamscores.Length; i++ ) {
             teamscores [ i ].scoreDisplay.color = teamscores [ i ].team.teamColor;
         }
+    }
+
+    private void SetPlayerControls () {
+        _playerOne.controlScheme = _controlSetup.playerOneControl;
+        _playerOne.gameObject.SetActive(_playerOne.CheckControlAvailability ());
+        _playerTwo.controlScheme = _controlSetup.playerTwoControl;
+        _playerTwo.gameObject.SetActive ( _playerTwo.CheckControlAvailability ());
+        _playerThree.controlScheme = _controlSetup.playerThreeControl;
+        _playerThree.gameObject.SetActive ( _playerThree.CheckControlAvailability ());
+        _playerFour.controlScheme = _controlSetup.playerFourControl;
+        _playerFour.gameObject.SetActive ( _playerFour.CheckControlAvailability ());
     }
 
     private void OnEnable () {
@@ -34,10 +61,8 @@ public class GameController : MonoBehaviour
     }
 
     void EvaluateGoal( TeamSettings.eTeam team ) {
-        //Debug.Log ( "GOAL CONTROLLER!" );
         for ( int i = 0; i< teamscores.Length; i++ )
         {
-            //Debug.Log ( "SHIT " + teamscores [ i ].team.team + " vs " + team );
             if ( teamscores[ i ].team.team != team ) {
                 teamscores [ i ].currentScore++;
                 teamscores [ i ].scoreDisplay.text = teamscores [ i ].currentScore.ToString();
